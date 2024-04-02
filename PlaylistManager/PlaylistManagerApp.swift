e2169132c8710b2fb4c7555ea18bc467d0a4c862
Yeffian <aditchakraborty2009@gmail.com>
2024-04-02 14:45:07 +0600

    Initial Commit

---
+++ PlaylistManager/PlaylistManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaylistManagerApp.swift
+//  PlaylistManager
+//
+//  Created by Adit Chakraborty on 02/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct PlaylistManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
